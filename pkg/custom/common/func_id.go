8ea41feab9c110d5a3af89c1db2a593330a1e06c
balckpixie <kuropixie@gmail.com>
2025-07-19 05:42:30 +0000

    mod 共通関数を整理

---
+++ pkg/custom/common/func_id.go
@@ -0,0 +1,80 @@
+package common
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+
+// DVD-ID形式をDMM形式に変換
+func ConvertFormat(input string) string {
+	re := regexp.MustCompile(`([a-zA-Z]{3,4})-(\d{3,})`)
+	matches := re.FindAllStringSubmatch(input, -1)
+
+	for _, match := range matches {
+		prefix := match[1]
+		numStr := match[2]
+
+		// 数字が4桁以上の場合はゼロパディングを行わない
+		num, err := strconv.Atoi(numStr)
+		if err == nil {
+			if num < 10000 {
+				numStr = fmt.Sprintf("%05d", num)
+			}
+		}
+		replacement := prefix + numStr
+		input = strings.Replace(input, match[0], replacement, -1)
+	}
+
+	return input
+}
+
+// DMM形式をDVD-ID形式に変換
+func ConvertToDVDId(input string) string {
+	re := regexp.MustCompile(`^([H_]*)(\d*)([a-zA-Z]+)(\d+)`)
+	matches := re.FindStringSubmatch(input)
+
+	if len(matches) == 5 {
+		prefix := strings.ToUpper(strings.Replace(matches[3], "_", "", 1))
+		num, err := strconv.Atoi(matches[4])
+		if err == nil {
+			numStr := fmt.Sprintf("%03d", num) // 3桁の数字に変換
+			return fmt.Sprintf("%s-%s", prefix, numStr)
+		}
+	}
+	return ""
+}
+
+func ExtractDVDIDLogic(filename string) string {
+	dvdid := ""
+	regex := regexp.MustCompile(`[a-zA-Z0-9]{2,6}-\d{2,6}`)
+	match := regex.FindString(filename)
+	if match == "" {
+		regex = regexp.MustCompile(`([a-zA-Z]{2,6})(\d{2,6})`)
+		match := regex.FindStringSubmatch(filename)
+		if match != nil {
+			firstPart := match[1]
+			secondPart := match[2]
+			if len(secondPart) >= 4 {
+				secondPart = strings.TrimLeft(secondPart, "0")
+			}
+			// 確認: secondPart が 3 文字未満の場合は 0 を補完する
+			if len(secondPart) < 3 {
+				secondPart = strings.Repeat("0", 3-len(secondPart)) + secondPart
+			}
+			dvdid = firstPart + "-" + secondPart
+		} else {
+			dvdid = "null"
+		}
+	} else {
+		dvdid = match
+	}
+	return dvdid
+}
+
+func ExtractFormat(input string) []string {
+	re := regexp.MustCompile(`[A-Z]{4}-\d{3,4}`)
+	return re.FindAllString(input, -1)
+}
---
+++ pkg/custom/common/func_query.go
@@ -0,0 +1,83 @@
+package common
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/xbapps/xbvr/pkg/config"
+)
+
+// URLとドメインの一致を確認
+func DomainMatch(urlString, pattern string) bool {
+	u, err := url.Parse(urlString)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return false
+	}
+	host := u.Hostname()
+
+	pattern = "^" + regexp.QuoteMeta(pattern) + "$"
+	pattern = strings.ReplaceAll(pattern, "\\*", "[^.]+") // ワイルドカードを正規表現に変換
+	re := regexp.MustCompile(pattern)
+	isMatch := re.MatchString(host)
+
+	return isMatch
+}
+
+// DMM API呼び出し時のQueryパラメータを追加
+func AddAPIParam(originalURL string) (string, error) {
+
+	if config.Config.Custom.DmmApiId == "" || config.Config.Custom.DmmAffiliateId == "" {
+		return "", errors.New("is not set DmmApiId and DmmAffiliateId param")
+	}
+
+	parsedURL, err := url.Parse(originalURL)
+	if err != nil {
+		return "", err
+	}
+
+	queryParams := parsedURL.Query()
+	queryParams.Set("api_id", config.Config.Custom.DmmApiId)
+	queryParams.Set("affiliate_id", config.Config.Custom.DmmAffiliateId)
+	parsedURL.RawQuery = queryParams.Encode()
+
+	return parsedURL.String(), nil
+}
+
+// Query Parameterを追加する
+func AddQueryParam(originalURL string, paramname string, value string) (string, error) {
+	parsedURL, err := url.Parse(originalURL)
+	if err != nil {
+		return "", err
+	}
+	queryParams := parsedURL.Query()
+	queryParams.Set(paramname, value)
+	parsedURL.RawQuery = queryParams.Encode()
+	return parsedURL.String(), nil
+}
+
+// Query Parameterの名前を置き換える
+func ReplaceQueryParam(originalURL string, paramname string, newParamname string) (string, error) {
+	parsedURL, err := url.Parse(originalURL)
+	if err != nil {
+		return "", err
+	}
+
+	queryParams := parsedURL.Query()
+	values := queryParams[paramname]
+	if len(values) > 0 {
+		// パラメーターが見つかった場合、新しいパラメーター名に置き換える
+		queryParams.Del(paramname)
+		for _, value := range values {
+			queryParams.Add(newParamname, value)
+		}
+		parsedURL.RawQuery = queryParams.Encode()
+	} else {
+		return "", errors.New("not found parameter")
+	}
+
+	return parsedURL.String(), nil
+}
