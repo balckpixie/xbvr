1b8f7139f1ff1f2e7ad427b941dda4dde0810536
balckpixie <kuropixie@gmail.com>
2025-07-30 15:32:10 +0000

    mot スプライト表示機能の実装

--- pkg/custom/api/custom_thumbnail.go
+++ pkg/custom/api/custom_thumbnail.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
+	"os"
+	"bytes"
 
 	restfulspec "github.com/emicklei/go-restful-openapi/v2"
 	"github.com/emicklei/go-restful/v3"
@@ -27,9 +29,35 @@ func (i ThumbnailResource) WebService() *restful.WebService {
 		ContentEncodingEnabled(false).
 		Metadata(restfulspec.KeyOpenAPITags, tags))
 
+			// HEAD: 存在確認のみ（ボディなし）
+	ws.Route(ws.HEAD("/image/{file-id}").To(i.headThumbnail).
+		Param(ws.PathParameter("file-id", "File ID").DataType("int")).
+		ContentEncodingEnabled(false).
+		Metadata(restfulspec.KeyOpenAPITags, tags))
+
 	return ws
 }
 func (i ThumbnailResource) getThumbnail(req *restful.Request, resp *restful.Response) {
 	fileID := req.PathParameter("file-id")
 	http.ServeFile(resp.ResponseWriter, req.Request, filepath.Join(common.VideoThumbnailDir, fmt.Sprintf("%v.jpg", fileID)))
+}
+
+func (i ThumbnailResource) headThumbnail(req *restful.Request, resp *restful.Response) {
+	fileID := req.PathParameter("file-id")
+	path := filepath.Join(common.VideoThumbnailDir, fmt.Sprintf("%v.jpg", fileID))
+
+	// ファイルの存在とメタデータを確認
+	info, err := os.Stat(path)
+	if err != nil {
+		if os.IsNotExist(err) {
+			resp.WriteErrorString(http.StatusNotFound, "Thumbnail not found")
+		} else {
+			resp.WriteErrorString(http.StatusInternalServerError, "Internal server error")
+		}
+		return
+	}
+
+	// 空の ReadSeeker を渡すことで ServeContent を正しく動作させる
+	emptyBody := bytes.NewReader([]byte{})
+	http.ServeContent(resp.ResponseWriter, req.Request, filepath.Base(path), info.ModTime(), emptyBody)
 }
